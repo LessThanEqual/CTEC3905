854b8e319a091df6be24a0c5a89940a8fb71958d
LessThanEqual <p14172850@my365.dmu.ac.uk>
2017-03-06 12:36:03 +0000

    added new JS function

--- assets/js/opendetails.js
+++ assets/js/opendetails.js
@@ -1,3 +1,9 @@
-function openArticle{
-	
+if (window.innerWidth > 768) {
+	var descriptions = document.getElementsByTagName('details');
+
+	for (i = 0; i < descriptions.length; i++) {
+		alert(descriptions[i]);
+        descriptions[i].setAttribute("open");  
+    }
+
 }
